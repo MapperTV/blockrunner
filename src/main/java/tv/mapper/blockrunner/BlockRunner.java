5204a167abb8bfc1b87a75a89afa2a455f278f96
Mapper <killermapper@outlook.com>
2020-12-06 13:25:47 +0100

    Initial commit

---
+++ src/main/java/tv/mapper/blockrunner/BlockRunner.java
@@ -0,0 +1,38 @@
+package tv.mapper.blockrunner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(BlockRunner.MODID)
+public class BlockRunner
+{
+    public static final String MODID = "blockrunner";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public BlockRunner()
+    {
+        // ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, BaseOreGenConfig.COMMON_CONFIG);
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::serverSetup);
+
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("Block Runner setup started!");
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event)
+    {}
+
+    private void serverSetup(final FMLDedicatedServerSetupEvent event)
+    {}
+}
